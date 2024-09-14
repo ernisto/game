5a539489008da6282db04e36b7df28f98a2efd3f
ernisto <joaovitordesouza707@gmail.com>
2024-09-14 10:48:43 -0300

    create default wally package folders

--- scripts/wally-install.bat
+++ scripts/wally-install.bat
@@ -1,4 +1,8 @@
 wally install
+mkdir Packages
+mkdir DevPackages
+mkdir ServerPackages
+
 call scripts/rojo-sourcemap
 wally-package-types --sourcemap sourcemap.json Packages
 wally-package-types --sourcemap sourcemap.json DevPackages
