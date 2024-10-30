c387705ba074b0bec6e2ab0c40f2c1ad968f494a
ernisto <joaovitordesouza707@gmail.com>
2024-10-29 23:48:40 -0300

    ci(scripts): trying sequential execution

--- scripts/wally-install.cmd
+++ scripts/wally-install.cmd
@@ -1,4 +1,6 @@
-mkdir Packages & mkdir DevPackages & mkdir ServerPackages & wally-patch-package & chmod +x scripts/rojo-sourcemap.cmd & scripts/rojo-sourcemap.cmd
+mkdir Packages & mkdir DevPackages & mkdir ServerPackages & wally-patch-package &
+chmod +x scripts/rojo-sourcemap.cmd; scripts/rojo-sourcemap.cmd
+
 wally-package-types --sourcemap sourcemap.json Packages
 wally-package-types --sourcemap sourcemap.json DevPackages
 wally-package-types --sourcemap sourcemap.json ServerPackages
