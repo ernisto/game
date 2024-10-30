6115665c231c347e80d31c40de8aa20bdf3c340c
ernisto <joaovitordesouza707@gmail.com>
2024-10-29 23:47:07 -0300

    ci(scripts): trying sequential execution

--- scripts/rojo-build.cmd
+++ scripts/rojo-build.cmd
@@ -1,1 +1,2 @@
-chmod +x scripts/wally-install.cmd & scripts/wally-install.cmd & mkdir -p out/rojo & rojo build game.project.json --output out/rojo/game.rbxl
+chmod +x scripts/wally-install.cmd
+scripts/wally-install.cmd & mkdir -p out/rojo & rojo build game.project.json --output out/rojo/game.rbxl
