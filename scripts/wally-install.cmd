9dc71efcfb532aa0efafd1b3fa51ce3ae6f91d29
ernisto <joaovitordesouza707@gmail.com>
2024-12-08 12:00:38 -0300

    ci(wally-install): remove Packages/init.luau

--- scripts/wally-install.cmd
+++ scripts/wally-install.cmd
@@ -4,11 +4,6 @@ mkdir DevPackages &
 mkdir ServerPackages &
 wally-patch-package &
 
-if %1 == dev.project.json goto skip
-echo --!nocheck> Packages/init.luau
-echo return require(script.package) >> Packages/init.luau
-:skip
-
 rojo sourcemap %1 -o sourcemap.json --include-non-scripts
 
 wally-package-types --sourcemap sourcemap.json Packages
