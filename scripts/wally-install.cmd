dcb26ad2674d0f4ea311b4f9adcfa5ad01afbe23
ernisto <joaovitordesouza707@gmail.com>
2024-12-08 12:05:30 -0300

    ci(wally-install): sourcemap DevPackages only with dev.project.json

--- scripts/wally-install.cmd
+++ scripts/wally-install.cmd
@@ -7,5 +7,8 @@ wally-patch-package &
 rojo sourcemap %1 -o sourcemap.json --include-non-scripts
 
 wally-package-types --sourcemap sourcemap.json Packages
-wally-package-types --sourcemap sourcemap.json DevPackages
 wally-package-types --sourcemap sourcemap.json ServerPackages
+
+if %1 == dev.project.json (
+    wally-package-types --sourcemap sourcemap.json DevPackages
+)
