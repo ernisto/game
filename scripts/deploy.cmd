6d173bff027ee7aa4f40062a25850abf7f88d025
ernisto <joaovitordesouza707@gmail.com>
2024-12-08 17:42:46 -0300

    ci(deploy): disable servers shutdown

--- scripts/deploy.cmd
+++ scripts/deploy.cmd
@@ -1,5 +1,1 @@
 mantle deploy
-curl -L -X POST 'https://apis.roblox.com/cloud/v2/universes/{universe}:restartServers'
-    -H 'x-api-key: {your-api-key}'
-    -H 'Content-Type: application/json'
-    --data '{}'
